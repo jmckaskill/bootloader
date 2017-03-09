947e99be49eb3c2ebea22d51a1d04565437fc0a7
James McKaskill <james_mckaskill@trimble.com>
2017-03-08 17:38:07 -0700

    add UART output

---
+++ deploy.bat
@@ -0,0 +1,3 @@
+
+dd.exe if=bootloader.MLO of=\\.\Volume{566389ba-da0e-11e3-a182-fda44de61971}
+dd.exe if=\\.\Volume{566389ba-da0e-11e3-a182-fda44de61971} of=bootloader.loaded count=2
--- tiimage.c
+++ tiimage.c
@@ -42,7 +42,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 
 typedef struct _ti_header_
